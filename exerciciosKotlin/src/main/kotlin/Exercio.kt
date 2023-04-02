4490f1d3394dcc51ad018326f6c942b57dfdba95
unknown <you@example.com>
2023-04-02 17:22:34 -0300

    exercicio 2

--- exerciciosKotlin/src/main/kotlin/Exercio.kt
+++ exerciciosKotlin/src/main/kotlin/Exercio.kt
@@ -1,2 +1,14 @@
-fun converterAnos(){
+fun converterAnos(anos: Int){
+    println(" $anos, anos equivalem a: ")
+    println(" ${anos *12} meses: ")
+    println(" ${anos *365} dias: ")
+    println(" ${anos *365 *24} horas: ")
+    println(" ${anos *365 *24*60} minutos: ")
+    println(" ${anos *365 *24*60*60} segundos: ")
+
+
+}
+fun main (){
+    (converterAnos(2))
+
 }
